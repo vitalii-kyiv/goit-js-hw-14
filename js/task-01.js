129dbc3b62d3e59cfe0024fedbd1817808c1516e
vitalii-kyiv <v.s.chernous@gmail.com>
2023-10-14 13:39:19 +0300

    Tasks1-5_vol2

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,8 @@ const allListItems = document.querySelectorAll(".item");
 console.log(`Number of categories: ${allListItems.length}`);
 
 allListItems.forEach((item) => {
-  const itemTitle = item.querySelector("h2");
-  const itemElements = item.querySelectorAll("li");
+  const itemTitle = item.firstElementChild;
+  const itemElements = itemTitle.nextElementSibling.children;
   console.log(`
   Category: ${itemTitle.textContent}
   Elements: ${itemElements.length}
--- js/task-02.js
+++ js/task-02.js
@@ -6,12 +6,13 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-
+const itemContents = [];
 const list = document.querySelector("#ingredients");
 
 ingredients.forEach((item) => {
   const itemContent = document.createElement("li");
   itemContent.textContent = item;
   itemContent.classList.add("item");
-  list.appendChild(itemContent);
+  itemContents.push(itemContent);
 });
+list.append(...itemContents);
